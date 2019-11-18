65d04be81c20d91782bbcc9c2489135ce6496c17
Turar <t@g.c>
2019-11-18 13:54:17 +0600

    few edition

--- inc/managerPage.php
+++ inc/managerPage.php
@@ -4,9 +4,9 @@
 	<meta charset="UTF-8">
 
 	<title>Document</title>
-	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	
 	<script type="text/javascript" src="../js/jquery-3.4.1.min.js"></script>
-	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+	
 	<script type="text/javascript" src="../js/managerPageScript.js"></script>
 	<link rel="stylesheet" type="text/css" href="../css/managerPageStyle.css">
 </head>
